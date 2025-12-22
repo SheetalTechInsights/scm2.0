3fe580246b0b5bf3d1d260c83c87eb0cbcb0e550
Sheetal Patidargit config --global user.email sheetalpatidar969@gmail.com <yourgithubemail@example.com>
2025-12-22 12:47:10 +0530

    Managing logged in user

--- src/main/java/com/scm/controllers/UserController.java
+++ src/main/java/com/scm/controllers/UserController.java
@@ -1,11 +1,20 @@
 package com.scm.controllers;
 
+import java.security.Principal;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.scm.helper.Helper;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
+
+    private Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @RequestMapping(value = "/dashboard")
     public String userDashboard() {
@@ -13,7 +22,10 @@ public class UserController {
     }
   
     @RequestMapping(value = "/profile")
-    public String userProfile() {
+    public String userProfile(Authentication authentication) {
+       String username =  Helper.getEmailOfLoggedInUser(authentication);
+       // String name = principal.getName();
+        logger.info("user logged in: {}" ,username);
         return "/user/profile";
     }
 }
